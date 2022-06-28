4609bdf744916099f2bb3cb671d89efb57c96e6b
Rabah <rmerzoug471@gmail.com>
2022-06-28 11:19:12 -0500

    finished the lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 24;
+const num4 = 5;
+const multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 10) + 1;
+let mod = num3 % num4;
+let max = Math.max(1, 2, 3, 4, 5, 12, 16, 20);
